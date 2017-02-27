e0b6c31f97a972afbfea38573594e47b69151a96
Ahmed Shams <ahmed.shams@hpi.de>
2017-02-27 16:46:35 +0100

    Chapter8 Complete with CSS and HTML

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  root :to => "products#index"
+  get 'store/index'
+  get 'store/contact'
+
+  #resources :store
+
+  root to: "store#index", as: 'Store'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
