b4b6bbd2b9eeac7024c446e1553815a3f81b3294
Ahmed Shams <ahmed.shams@hpi.de>
2017-11-23 15:24:19 +0100

    clean chapter 12

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,6 @@ class Order < ApplicationRecord
 
   validates :name, :address, :email, presence: true
   validates :pay_type, inclusion: PAYMENT_TYPES
-  validates_presence_of :pay_type_id
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
